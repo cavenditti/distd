8c8947f026df16e6ea7c3801bcbb96b4bab49b1a
Carlo Antonio Venditti <c.a.venditti@pm.me>
2024-10-30 17:08:35 +0100

    Add test for core.utils.uuid functions

--- core/src/utils/uuid.rs
+++ core/src/utils/uuid.rs
@@ -10,3 +10,21 @@ pub fn bytes_to_uuid(x: Bytes) -> Uuid {
 pub fn slice_to_uuid(x: &[u8]) -> Uuid {
     Uuid::from_bytes(*x.array_chunks::<16>().collect::<Vec<&[u8; 16]>>()[0])
 }
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+    use uuid::Uuid;
+
+    use super::bytes_to_uuid;
+    use super::slice_to_uuid;
+
+    #[test]
+    fn test_bytes_to_uuid() {
+        let uuid = Uuid::new_v4();
+        let slice = uuid.as_bytes().as_slice();
+        let bytes = Bytes::copy_from_slice(slice);
+        assert_eq!(bytes_to_uuid(bytes), uuid);
+        assert_eq!(slice_to_uuid(slice), uuid);
+    }
+}
