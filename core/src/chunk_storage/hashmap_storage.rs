52ba66bfa881f928c7ef6c2dff7d651a1578293f
Carlo Antonio Venditti <c.a.venditti@pm.me>
2026-03-07 19:53:48 +0100

    refactor(core): HashMapStorage - interior mutability with Arc<RwLock<_>>
    Replaces the plain FxHashMap field with Arc<RwLock<FxHashMap<...>>> so
    all ChunkStorage methods can take &self. Clone is implemented as an
    Arc clone (shared map) rather than a deep copy.
    
    store_chunk and store_link now use an explicit entry check + insert
    instead of the unstable map_try_insert feature.

--- core/src/chunk_storage/hashmap_storage.rs
+++ core/src/chunk_storage/hashmap_storage.rs
@@ -1,5 +1,5 @@
 use rustc_hash::FxHashMap;
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 use crate::chunk_storage::ChunkStorage;
 use crate::hash::{Hash, HashTreeCapable};
@@ -7,64 +7,62 @@ use crate::hash::{Hash, HashTreeCapable};
 use super::{Node, StorageError};
 
 /// Dead simple in-memory global storage
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default)]
 pub struct HashMapStorage {
-    data: FxHashMap<Hash, Arc<Node>>,
+    data: Arc<RwLock<FxHashMap<Hash, Arc<Node>>>>,
+}
+
+/// Cloning shares the underlying map (Arc clone).
+impl Clone for HashMapStorage {
+    fn clone(&self) -> Self {
+        Self {
+            data: Arc::clone(&self.data),
+        }
+    }
 }
 
 impl ChunkStorage for HashMapStorage {
     fn get(&self, hash: &Hash) -> Option<Arc<Node>> {
-        self.data.get(hash).cloned()
+        self.data.read().unwrap().get(hash).cloned()
     }
 
-    fn store_chunk(&mut self, hash: Hash, chunk: &[u8]) -> Option<Arc<Node>> {
-        //println!("[StorageInsert] Hash: {}, size: {}", hash, size);
-        if let Some(raw_chunk) = self.data.get(&hash) {
-            return Some(raw_chunk.clone());
+    fn store_chunk(&self, hash: Hash, chunk: &[u8]) -> Result<Arc<Node>, StorageError> {
+        let mut data = self.data.write().unwrap();
+        if let Some(node) = data.get(&hash) {
+            return Ok(node.clone());
         }
-        self.data
-            .try_insert(
-                hash,
-                Arc::new(Node::Stored {
-                    hash,
-                    data: Arc::new(Vec::from(chunk)),
-                }),
-            )
-            .ok()
-            .cloned()
+        let node = Arc::new(Node::Stored {
+            hash,
+            data: Arc::new(Vec::from(chunk)),
+        });
+        data.insert(hash, node.clone());
+        Ok(node)
     }
 
-    fn store_link(&mut self, hash: Hash, left: Arc<Node>, right: Arc<Node>) -> Option<Arc<Node>> {
-        /*
-        println!(
-            "[Storage Link ]: {}: {} + {}",
-            hash,
-            left.hash(),
-            right.hash()
-        );
-        */
+    fn store_link(&self, hash: Hash, left: Arc<Node>, right: Arc<Node>) -> Result<Arc<Node>, StorageError> {
+        let mut data = self.data.write().unwrap();
+        if let Some(node) = data.get(&hash) {
+            return Ok(node.clone());
+        }
         let size = left.size() + right.size();
-        self.data.get(&hash).cloned().or(self
-            .data
-            .try_insert(
-                hash,
-                Arc::new(Node::Parent {
-                    hash,
-                    size,
-                    left,
-                    right,
-                }),
-            )
-            .ok()
-            .map(|x| x.clone()))
+        let node = Arc::new(Node::Parent {
+            hash,
+            size,
+            left,
+            right,
+        });
+        data.insert(hash, node.clone());
+        Ok(node)
     }
 
     fn chunks(&self) -> Vec<Hash> {
-        self.data.keys().copied().collect()
+        self.data.read().unwrap().keys().copied().collect()
     }
 
     fn size(&self) -> u64 {
         self.data
+            .read()
+            .unwrap()
             .values()
             .map(|x| match &**x {
                 Node::Stored { data, .. } => data.len() as u64,
@@ -75,16 +73,12 @@ impl ChunkStorage for HashMapStorage {
 }
 
 impl HashTreeCapable<Arc<Node>, crate::error::Error> for HashMapStorage {
-    fn func(&mut self, data: &[u8]) -> Result<Arc<Node>, crate::error::Error> {
-        Ok(self
-            .insert_chunk(data)
-            .ok_or(StorageError::ChunkInsertError)?)
+    fn func(&self, data: &[u8]) -> Result<Arc<Node>, crate::error::Error> {
+        self.insert_chunk(data).map_err(Into::into)
     }
 
-    fn merge(&mut self, l: &Arc<Node>, r: &Arc<Node>) -> Result<Arc<Node>, crate::error::Error> {
-        Ok(self
-            .link(l.clone(), r.clone())
-            .ok_or(StorageError::LinkCreation)?)
+    fn merge(&self, l: &Arc<Node>, r: &Arc<Node>) -> Result<Arc<Node>, crate::error::Error> {
+        self.link(l.clone(), r.clone()).map_err(Into::into)
     }
 }
 
