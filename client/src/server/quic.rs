d102b3d79e85d899533b57296c3a873981d406fb
Carlo Antonio Venditti <c.a.venditti@pm.me>
2026-03-15 22:19:48 +0100

    Fix QUIC sync manifest framing

--- client/src/server/quic.rs
+++ client/src/server/quic.rs
@@ -1,13 +1,14 @@
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::sync::Arc;
 
-use distd_core::proto::{ClientKeepAlive, ClientRegister, ManifestRequest, ManifestResponse, PossessionBitfield, ServerMetadata, SyncMessage};
+use distd_core::proto::{sync_message::Msg, ClientKeepAlive, ClientRegister, ManifestRequest, ManifestResponse, PossessionBitfield, ServerMetadata, SyncMessage};
 use distd_core::transport::TransportOp;
 use distd_core::utils::frame::{read_length_delimited_async, write_length_delimited_async, write_optional_uuid_async, write_transport_op_async};
 use quinn::{ClientConfig, Endpoint};
 use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
 use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
 use rustls::{DigitallySignedStruct, Error as TlsError, SignatureScheme};
+use tokio::io::AsyncRead;
 use tokio::sync::RwLock;
 use uuid::Uuid;
 
@@ -191,11 +192,24 @@ impl QuicTransportClient {
             .await
             .map_err(|err| ServerRequest::Quic(err.to_string()))?;
 
-        let manifest = read_length_delimited_async(&mut recv)
-            .await
-            .map_err(|err| ServerRequest::Quic(err.to_string()))?;
+        let manifest = read_sync_manifest_response(&mut recv).await?;
 
         Ok((manifest, QuicSyncSession { send, recv }))
+    }
+}
+
+async fn read_sync_manifest_response<R>(reader: &mut R) -> Result<ManifestResponse, ServerRequest>
+where
+    R: AsyncRead + Unpin,
+{
+    match read_length_delimited_async::<_, SyncMessage>(reader)
+        .await
+        .map_err(|err| ServerRequest::Quic(err.to_string()))?
+    {
+        SyncMessage {
+            msg: Some(Msg::ManifestResponse(response)),
+        } => Ok(response),
+        _ => Err(ServerRequest::UnexpectedMessage),
     }
 }
 
@@ -220,5 +234,71 @@ impl QuicSyncSession {
         }
 
         Ok(responses)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::read_sync_manifest_response;
+    use distd_core::proto::{
+        sync_message::Msg, ChunkData, ManifestResponse, PayloadCompression, SyncMessage,
+    };
+    use distd_core::utils::frame::write_length_delimited_async;
+    use tokio::io::duplex;
+
+    #[tokio::test]
+    async fn reads_manifest_response_from_sync_message_frame() {
+        let (mut writer, mut reader) = duplex(4096);
+        let expected = ManifestResponse {
+            artifact_id: "artifact-a".to_string(),
+            version: 7,
+            root_hash: vec![1, 2, 3, 4],
+            total_size: 42,
+            chunk_count: 1,
+            chunk_size: 256,
+            chunk_hashes: vec![vec![9; 32]],
+            entries: Vec::new(),
+            chunk_algorithm: None,
+            chunk_sizes: vec![42],
+        };
+        let expected_for_writer = expected.clone();
+
+        tokio::spawn(async move {
+            let frame = SyncMessage {
+                msg: Some(Msg::ManifestResponse(expected_for_writer)),
+            };
+            write_length_delimited_async(&mut writer, &frame)
+                .await
+                .expect("write sync manifest frame");
+        });
+
+        let actual = read_sync_manifest_response(&mut reader)
+            .await
+            .expect("decode manifest response");
+        assert_eq!(actual, expected);
+    }
+
+    #[tokio::test]
+    async fn rejects_non_manifest_sync_message_frame() {
+        let (mut writer, mut reader) = duplex(4096);
+
+        tokio::spawn(async move {
+            let frame = SyncMessage {
+                msg: Some(Msg::ChunkData(ChunkData {
+                    chunk_index: 0,
+                    data: vec![1, 2, 3],
+                    compression: PayloadCompression::Unspecified as i32,
+                    uncompressed_size: 3,
+                })),
+            };
+            write_length_delimited_async(&mut writer, &frame)
+                .await
+                .expect("write non-manifest frame");
+        });
+
+        let err = read_sync_manifest_response(&mut reader)
+            .await
+            .expect_err("non-manifest frame should fail");
+        assert!(matches!(err, crate::error::ServerRequest::UnexpectedMessage));
     }
 }
